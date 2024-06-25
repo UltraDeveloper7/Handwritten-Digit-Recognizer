e07455f1731894bb1f767dbbdd93573d16527f99
UltraDeveloper7 <75303541+UltraDeveloper7@users.noreply.github.com>
2024-06-25 10:49:44 +0300

    Update train_digit_recognizer.py

--- train_digit_recognizer.py
+++ train_digit_recognizer.py
@@ -3,7 +3,8 @@ from keras.datasets import mnist # type: ignore
 from keras.models import Sequential # type: ignore
 from keras.layers import Dense, Dropout, Flatten # type: ignore
 from keras.layers import Conv2D, MaxPooling2D # type: ignore
-from keras import backend as K
+from keras.optimizers import Adam # type: ignore
+from keras.utils import to_categorical # type: ignore
 
 class MNISTModel:
     def __init__(self):
@@ -13,8 +14,8 @@ class MNISTModel:
         self.x_test = self.x_test.reshape(self.x_test.shape[0], 28, 28, 1)
         self.input_shape = (28, 28, 1)
 
-        self.y_train = keras.utils.to_categorical(self.y_train, 10)
-        self.y_test = keras.utils.to_categorical(self.y_test, 10)
+        self.y_train = to_categorical(self.y_train, 10)
+        self.y_test = to_categorical(self.y_test, 10)
 
         self.x_train = self.x_train.astype('float32')
         self.x_test = self.x_test.astype('float32')
@@ -23,25 +24,26 @@ class MNISTModel:
 
         self.batch_size = 128
         self.num_classes = 10
-        self.epochs = 10
+        self.epochs = 20 
 
         self.model = None
 
     def build_model(self):
         self.model = Sequential()
-        self.model.add(Conv2D(32, kernel_size=(5, 5), activation='relu', input_shape=self.input_shape))
+        self.model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=self.input_shape))
+        self.model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
         self.model.add(MaxPooling2D(pool_size=(2, 2)))
-        self.model.add(Conv2D(64, (3, 3), activation='relu'))
+        self.model.add(Dropout(0.25))
+        self.model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
         self.model.add(MaxPooling2D(pool_size=(2, 2)))
+        self.model.add(Dropout(0.25))
         self.model.add(Flatten())
-        self.model.add(Dense(128, activation='relu'))
-        self.model.add(Dropout(0.3))
-        self.model.add(Dense(64, activation='relu'))
+        self.model.add(Dense(256, activation='relu'))
         self.model.add(Dropout(0.5))
         self.model.add(Dense(self.num_classes, activation='softmax'))
 
-        self.model.compile(loss=keras.losses.categorical_crossentropy,
-                           optimizer=keras.optimizers.Adadelta(),
+        self.model.compile(loss='categorical_crossentropy',
+                           optimizer=Adam(),
                            metrics=['accuracy'])
 
     def train_model(self):
@@ -74,5 +76,9 @@ class MNISTModel:
         self.train_model()
         self.evaluate_model()
         self.save_model()
+
+if __name__ == "__main__":
+    mnist_model = MNISTModel()
+    mnist_model.run()
 
 
